0a1340e7e8041440db9f83453f91572ae6b0d78f
Osiris8 <miganosiris8@gmaaail.com>
2025-05-16 22:54:20 +0200

    set list display

--- members/models.py
+++ members/models.py
@@ -4,4 +4,6 @@ class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
 
-# Create your models here.
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
